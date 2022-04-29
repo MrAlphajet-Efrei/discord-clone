226a87c2b098f3716c0542f1188606fc55c21740
MrAlphajet-Efrei <yannick800@outlook.fr>
2022-04-29 20:18:03 +0200

    first commit

---
+++ discord_api/src/main/java/com/example/discord_api/DiscordApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.discord_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordApiApplication.class, args);
+    }
+
+}
